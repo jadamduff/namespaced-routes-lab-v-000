2b8f8ab91b702b935f5a4cccd4a48cd4c941fc9f
Adam Duff <j.adam.duff@gmail.com>
2018-10-02 16:12:41 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,15 +4,15 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    if Preference.last.allow_create_artists == true
-      @artist = Artist.find(params[:id])
-    else
-      redirect_to artists_path
-    end
+    @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    if Preference.last.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
